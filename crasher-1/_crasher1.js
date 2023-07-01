2b14d49ca155a6c6297c5b8609c37dbdfdf23591
ArmoredDragon <43324896+Armored-Dragon@users.noreply.github.com>
2023-07-01 14:58:32 -0500

    Removed unneeded Messages.subscribe More comments. More specific variable names.

--- crasher-1/_crasher1.js
+++ crasher-1/_crasher1.js
@@ -1,38 +1,44 @@
 (function () {
+  // When I was working on my chat app, I tried to toggle between displaying a new overlay window, or using the tablet.
+  // This attempt caused a hard crash.
+
   var app_is_visible = true;
   var AppUi = Script.require("appUi");
-  var dummy_variable = false;
+  var dummy_variable = false; // Intention: Whether or not the app should render in an overlay or the tablet
 
   // Global vars
-  var ac_tablet;
-  var chat_overlay_window;
+  var tablet;
+  var overlay_window;
 
   startup();
 
   function startup() {
-    ac_tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+    tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
 
-    var app_button = ac_tablet.addButton({
+    var app_button = tablet.addButton({
       icon: Script.resolvePath("./img/icon.png"),
       text: "crsh1"
     });
 
     // When script ends, remove itself from tablet
     Script.scriptEnding.connect(function () {
-      ac_tablet.removeButton(app_button);
-      chat_overlay_window.close();
+      tablet.removeButton(app_button);
+      overlay_window.close();
     });
 
+    // if dummy_variable is false, use the overlay
     if (!dummy_variable) {
-      chat_overlay_window = new OverlayWebWindow({
+      overlay_window = new OverlayWebWindow({
         title: "crsh1",
         width: 350,
         height: 400,
         visible: app_is_visible,
         source: Script.resolvePath("./index.html")
       });
-    } else {
-      chat_overlay_window = new AppUi({
+    }
+    // If dummy_variable is true, use tablet
+    else {
+      overlay_window = new AppUi({
         buttonName: "crsh1",
         home: Script.resolvePath("./index.html"),
         graphicsDirectory: Script.resolvePath("./") // The path to your button icons
@@ -41,22 +47,17 @@
 
     // Overlay button toggle
     app_button.clicked.connect(toggleMainChatWindow);
-    chat_overlay_window.closed.connect(toggleMainChatWindow);
+    overlay_window.closed.connect(toggleMainChatWindow);
 
     function toggleMainChatWindow() {
       app_is_visible = !app_is_visible;
       app_button.editProperties({ isActive: app_is_visible });
-      chat_overlay_window.visible = app_is_visible;
+      overlay_window.visible = app_is_visible;
     }
   }
 
-  // Initialize default message subscriptions
-  Messages.subscribe("chat");
-  Messages.subscribe("system");
-
   // Add event listeners
-  // FIXME: below needs to be changed to work with appui
-  chat_overlay_window.webEventReceived.connect(onWebEventReceived);
+  overlay_window.webEventReceived.connect(onWebEventReceived);
 
   // function receivedMessage(channel, message) {}
   function onWebEventReceived(event) {
@@ -64,7 +65,7 @@
     event = JSON.parse(event);
     if (event.action === "change_to_true") {
       dummy_variable = true;
-      chat_overlay_window.close();
+      overlay_window.close();
     }
   }
 })();
